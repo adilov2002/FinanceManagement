149c45c5905ed1f2f366722337c6ad7b464580d0
Sabyr <shatekov.sabyr@gmail.com>
2021-11-17 22:47:35 +0600

    Added models

--- myapp/models.py
+++ myapp/models.py
@@ -1,9 +1,34 @@
 from django.db import models
 
 
-class Users(models.Model):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
-    phone = models.CharField(max_length=15)
-    email = models.CharField(max_length=50)
+class Roles(models.Model):
+    """Roles"""
+    name = models.CharField("name", max_length=150)
 
+
+class Users(models.Model):
+    """Users"""
+    name = models.CharField("name", max_length=100)
+    surname = models.CharField("surname", max_length=100)
+    email = models.EmailField()
+    password = models.CharField("password", max_length=10)
+    role = models.ForeignKey(Roles, verbose_name="Role", on_delete=models.CASCADE)
+
+
+class Categories(models.Model):
+    """Categories"""
+    name = models.CharField("name", max_length=100)
+
+
+class Items(models.Model):
+    """Items"""
+    name = models.CharField("name", max_length=100)
+    price = models.PositiveSmallIntegerField("price", default=0)
+    category = models.ForeignKey(Categories, verbose_name="Category", on_delete=models.CASCADE)
+
+
+class Purchases(models.Model):
+    """Purchases"""
+    user = models.ForeignKey(Users, verbose_name="User", on_delete=models.CASCADE)
+    item = models.ForeignKey(Items, verbose_name="Item", on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
