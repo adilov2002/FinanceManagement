813049e1a2615fea08692b25d74a08985997b4e9
ZhanniyetAd <joni.2002.08@gmail.com>
2021-12-19 11:45:23 +0600

    fixed purchases date column

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, User
+from django.utils import timezone
 
 
 class Categories(models.Model):
@@ -33,7 +34,7 @@ class Purchases(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
     item = models.ForeignKey(Items, verbose_name="Item", on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateField(default=timezone.now)
 
     def __str__(self):
         return self.user.username + '_' + self.item.name + '_' + self.date.__str__() + '_' + self.item.price.__str__()
